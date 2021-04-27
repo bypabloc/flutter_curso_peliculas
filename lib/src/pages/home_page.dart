34066a6064c216d031da2f0d8d4f40920b8c513c
Pablo Contreras <pacg1991@gmail.com>
2021-04-26 23:53:52 -0500

    card swiper n model pelicula

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
-
-import 'package:flutter/material.dart';
+import 'package:flutter_curso_peliculas/src/widgets/card_swiper.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Text('Hola mundo')),
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text('Peliculas en cines'),
+        backgroundColor: Colors.indigoAccent,
+        actions: [IconButton(icon: Icon(Icons.search), onPressed: () {})],
+      ),
+      body: Container(
+        child: Column(
+          children: [_swiperCards()],
+        ),
+      ),
     );
+  }
+
+  Widget _swiperCards() {
+    return CardSwiper(peliculas: [1, 2, 3, 4, 5]);
   }
 }
